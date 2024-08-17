6b7a5512c4680a9a701345bb8a413230d0b9f35f
diegorezm <dmourarezende@gmail.com>
2024-08-17 17:53:03 -0300

    added zod

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,58 +1,69 @@
-import { Request, Response } from 'express'
-import { BadRequestError } from '../helpers/api-error'
-import { userRepository } from '../repositories/userRepository'
-import bcrypt from 'bcrypt'
+import { Request, Response } from "express";
+import { BadRequestError } from "../helpers/api-error";
+import { userRepository } from "../repositories/userRepository";
+import bcrypt from "bcrypt";
+import { z } from "zod";
 
+const userInsertSchema = z.object({
+  name: z.string().min(4).max(255),
+  email: z.string().email(),
+  password: z.string(),
+});
+
+const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
 export class UserController {
-	async create(req: Request, res: Response) {
-		const { name, email, password } = req.body
+  async create(req: Request, res: Response) {
+    const body = req.body;
+    const { email, name, password } = userInsertSchema.parse(body);
 
-		const userExists = await userRepository.findOneBy({ email })
+    const userExists = await userRepository.findOneBy({
+      email,
+    });
 
-		if (userExists) {
-			throw new BadRequestError('E-mail já existe')
-		}
+    if (userExists) {
+      throw new BadRequestError("E-mail já existe");
+    }
 
-		const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = await bcrypt.hash(password, 10);
 
-		const newUser = userRepository.create({
-			name,
-			email,
-			password: hashPassword,
-		})
+    const newUser = userRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
 
-		await userRepository.save(newUser)
+    await userRepository.save(newUser);
 
-		const { password: _, ...user } = newUser
+    const { password: _, ...user } = newUser;
 
-		return res.status(201).json(user)
-	}
+    return res.status(201).json(user);
+  }
 
-	async login(req: Request, res: Response) {
-		const { email, password } = req.body
+  async login(req: Request, res: Response) {
+    const body = req.body;
+    const { email, password } = userLoginSchema.parse(body);
 
-		const user = await userRepository.findOneBy({ email })
+    const user = await userRepository.findOneBy({ email });
 
-		if (!user) {
-			throw new BadRequestError('E-mail ou senha inválidos')
-		}
+    if (!user) {
+      throw new BadRequestError("E-mail ou senha inválidos");
+    }
 
-		const verifyPass = await bcrypt.compare(password, user.password)
+    const verifyPass = await bcrypt.compare(password, user.password);
 
-		if (!verifyPass) {
-			throw new BadRequestError('E-mail ou senha inválidos')
-		}
+    if (!verifyPass) {
+      throw new BadRequestError("E-mail ou senha inválidos");
+    }
 
-		
+    const { password: _, ...userLogin } = user;
 
-		const { password: _, ...userLogin } = user
-
-		return res.json({
-			user: userLogin,
-			
-		})
-	}
-
-	
+    return res.json({
+      user: userLogin,
+    });
+  }
 }
+
